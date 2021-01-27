11d9c338bc9055810c6b68ed357058c3344fa8d1
Hanna Munoz <hannamunoz@gmail.com>
2021-01-26 22:03:24 -0500

    adding more javascript

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $( document ).ready(function() {
-    let endpoint = 'https://api.linkpreview.net'
-    let apiKey = '5b578yg9yvi8sogirbvegoiufg9v9g579gviuiub8'
+//     let endpoint = 'https://api.linkpreview.net'
+//     let apiKey = '5b578yg9yvi8sogirbvegoiufg9v9g579gviuiub8 13659ca4a2177fdbcfbf8f91bf870bd9'
   
-    $( ".content a" ).each(function( index, element ) {
+//     $( ".content a" ).each(function( index, element ) {
   
-      $.ajax({
-          url: endpoint + "?key=" + apiKey + " &q=" + $( this ).text(),
-          contentType: "application/json",
-          dataType: 'json',
-          success: function(result){
-              console.log(result);
-          }, error: function(error) {
-            console.log(error);
-        }
-      })
-    });
-  });
+//       $.ajax({
+//           url: endpoint + "?key=" + apiKey + " &q=" + $( this ).text(),
+//           contentType: "application/json",
+//           dataType: 'json',
+//           success: function(result){
+//               console.log(result);
+//           }, error: function(error) {
+//             console.log(error);
+//         }
+//       })
+//     });
+//   });
 
   var searchBtn = document.getElementById('searchButton');
   var searchInput = document.getElementById('searchValue');
@@ -24,6 +24,16 @@ $( document ).ready(function() {
   var humidity = document.getElementById('humid');
   var wind = document.getElementById('wind');
   var uvIndex = document.getElementById('uv');
+  var searchHist = [];
 
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&${apiKey}}`)
-    .then(response = response.json)
+searchBtn.addEventListener('click', function(){
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+searchInput.value+'&appid=13659ca4a2177fdbcfbf8f91bf870bd9')
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+.catch(error => alert('Please enter a city name'))
+})
+
+});
+
+// need an onclick for searchBtn that updates the history
