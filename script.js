62016030ca29165c18542dfb063aca9de30e9901
Hanna Munoz <hannamunoz@gmail.com>
2021-01-27 13:21:47 -0500

    adding javascript functions

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
 $( document ).ready(function() {
-//     let endpoint = 'https://api.linkpreview.net'
-//     let apiKey = '5b578yg9yvi8sogirbvegoiufg9v9g579gviuiub8 13659ca4a2177fdbcfbf8f91bf870bd9'
-  
-//     $( ".content a" ).each(function( index, element ) {
-  
-//       $.ajax({
-//           url: endpoint + "?key=" + apiKey + " &q=" + $( this ).text(),
-//           contentType: "application/json",
-//           dataType: 'json',
-//           success: function(result){
-//               console.log(result);
-//           }, error: function(error) {
-//             console.log(error);
-//         }
-//       })
-//     });
-//   });
 
   var searchBtn = document.getElementById('searchButton');
   var searchInput = document.getElementById('searchValue');
@@ -25,15 +8,36 @@ $( document ).ready(function() {
   var wind = document.getElementById('wind');
   var uvIndex = document.getElementById('uv');
   var searchHist = [];
+  var apiKey = '13659ca4a2177fdbcfbf8f91bf870bd9'
 
-searchBtn.addEventListener('click', function(){
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+searchInput.value+'&appid=13659ca4a2177fdbcfbf8f91bf870bd9')
-    .then(response => response.json())
-    .then(data => console.log(data))
-
-.catch(error => alert('Please enter a city name'))
-})
-
-});
 
 // need an onclick for searchBtn that updates the history
+// $(searchBtn).on('click', updateHistory);
+
+// Need to add onclick for history cities to be able to click
+
+// need a call to the API
+function searchForWeather() {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}`;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        error: function(err) {
+            errorModal(err);
+        }
+    }).then(function(response) {
+        console.log(response);
+        // Need a function here to update all the information on the page
+    })
+}
+});
+
+function updateWeather(data) {
+    // Need to update the time to current timezone else current days could get confused
+    let rawTimezone = parseInt(data.dt) + parseInt(data.timezone);
+    let ourTimezone = DateTime.fromSeconds(rawTimezone);
+
+    // Set weather information to locations on html
+    cityName.innerHTML = data.name + ' ' + ourTimezone.month + '/' + ourTimezone.day + '/' + ourTimezone.year;
+}
