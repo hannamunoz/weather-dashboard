cda3722835a07facd7a2684c81d87297db1298e1
Hanna Munoz <hannamunoz@gmail.com>
2021-01-31 21:32:28 -0500

    finishing localStorage and page styling

--- script.js
+++ script.js
@@ -6,27 +6,28 @@ $(document).ready(function () {
     let searchInput = document.getElementById('searchValue');
     // Update areas on page
     let cityName = document.getElementById('cityName');
+    let weatherIcon = document.getElementById('icon')
     let temp = document.getElementById('temp');
     let humidity = document.getElementById('humid');
     let wind = document.getElementById('wind');
     let uvIndex = document.getElementById('uv');
     // 5 Day Forecast areas to update
-    let forecastFive = document.getElementById('forecastFive');
+    let forecastFive = document.getElementById('fiveDayCard');
     // Array to store search history
-    var searchHist = [];
-    // ErrorModal contents
-    let errorModal = document.getElementById('errorModal');
-    let modalClose = document.getElementById('modalClose');
-    let errorContent = document.getElementById('errorContent');
+    let searchHist = [];
+    let historyPanel = document.getElementById('cityHistory');
     // API key
     const apiKey = '13659ca4a2177fdbcfbf8f91bf870bd9'
     // Using Luxon for Date/Time
     let dateTime = luxon.DateTime;
 
+    // load history on refresh of page
+    loadCityHistory();
 
     // need an onclick for searchBtn that updates the history
     $(searchBtn).on('click', (e) => {
         e.preventDefault();
+        updateHistory(searchInput.value.trim());
         searchFormInput();
     });
 
@@ -34,10 +35,9 @@ $(document).ready(function () {
 
     // Need function to gather data from search input 
     function searchFormInput() {
-        let cityInput = $(searchInput).val().trim();
+        let cityInput = searchInput.value.trim();
         searchInput.value = "";
         if (!cityInput) {
-            // Need to call function for modal
         } else {
             searchForWeather(cityInput);
         }
@@ -49,10 +49,7 @@ $(document).ready(function () {
 
         $.ajax({
             url: queryURL,
-            method: "GET",
-            error: function (err) {
-                errorModal(err);
-            }
+            method: "GET"
         }).then(function (response) {
             console.log(response);
             // Function from below to update data
@@ -68,6 +65,8 @@ $(document).ready(function () {
 
         // Set weather information to locations on html
         cityName.innerHTML = data.name + ' ' + ourTimezone.month + '/' + ourTimezone.day + '/' + ourTimezone.year;
+        weatherIcon.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+        weatherIcon.alt = data.weather[0].description + " weather icon";
         temp.innerHTML = 'Temperature: ' + data.main.temp + 'F°';
         humidity.innerHTML = 'Humidity: ' + data.main.humidity + '%';
         wind.innerHTML = 'Wind Speed: ' + data.wind.speed + 'MPH';
@@ -77,19 +76,33 @@ $(document).ready(function () {
 
         $.ajax({
             url: urlQuery,
-            method: 'GET',
-            error: function (err) {
-                errorModal(err);
-            }
-        }).then(function (response) {
+            method: 'GET'
+        }).then(function (data) {
             // need another function to update the 5 day forecast & uv index (not on original call)
-            updateFiveDay(response);
+            updateFiveDay(data);
         });
 
-    
+
     }
 
     // Need a function to load search history 
+    function loadCityHistory() {
+        // parse out data
+        let storedHistory = JSON.parse(localStorage.getItem("weatherHistory"));
+        if (!storedHistory || storedHistory.length == 0) {
+            return;
+        } else {
+            storedHistory.forEach((data) => {
+                searchHist = [...storedHistory];
+            });
+        }
+        for (i = 0; i < 5; i++) {
+            if (searchHist[i] != undefined) {
+                $(historyPanel).append(`<div class="border p-2" id="cityHistory">${searchHist[i]}</div>`);
+            }
+        }
+        searchForWeather(searchHist[0], false);
+    }
 
     // function to update the five day weather and UV index
     function updateFiveDay(data) {
@@ -100,7 +113,7 @@ $(document).ready(function () {
         if (uvIndexInfo <= 3) {
             $(uvIndex).attr("class", "highlightGreen");
         } else if (3 < uvIndexInfo && uvIndexInfo <= 6) {
-            $(uvIndex).attr("class", "highlightYellow"); 
+            $(uvIndex).attr("class", "highlightYellow");
         } else {
             $(uvIndex).attr("class", "highlightRed");
         }
@@ -116,21 +129,26 @@ $(document).ready(function () {
             //  Need to parse the time data to readable units
             let myTimezone = dateTime.fromSeconds(thatTimezone);
             // Append returned info to fiveday forecast panels
-            $(forecastFive).append(`<div class="fiveDayCard flexcol col-1 rounded" id="card${i}"></div>`);
+            $(forecastFive).append(`<section class="rounded border flexCol fiveDayPanel" id="card${i}"></section>`);
             // Add details of forecast
-            $('#card' + i).append(`<h3 id="forecastFive" class="text-white h4 mb-3">${myTimezone.month}/${myTimezone.day}/${myTimezone.year}</h3>`)
-            // Need to add images here
-            // adding the temperature
-            .append(`<p class="text-white">Temp: ${fiveDayData.temp.day}F°</p>`)
-            // adding humidity
-            .append(`<p class="text-white mb-0">Humidity: ${fiveDayData.humidity}</p>`)
+            $('#card' + i).append(`<h3 class="forecastFiveDay">${myTimezone.month}/${myTimezone.day}/${myTimezone.year}</h3>`)
+                // add images here
+                .append(`<img id="fiveDayIcon" src="http://openweathermap.org/img/wn/${fiveDayData.weather[0].icon}.png" alt="${fiveDayData.weather[0].description} weather icon"/>`)
+                // adding the temperature
+                .append(`<article class="forecastFiveDay">Temp: ${fiveDayData.temp.day}F°</article>`)
+                // adding humidity
+                .append(`<article class="forecastFiveDay mb-0">Humidity: ${fiveDayData.humidity}</article>`);
 
         }
     }
 
     // Function to update the city history
     function updateHistory(city) {
-        
+        console.log(city);
+        $(historyPanel).prepend(`<div class="border p-2" id="cityHistory">${city}</div>`);
+        // Add city to history array
+        searchHist.unshift(city);
+        localStorage.setItem("weatherHistory", JSON.stringify(searchHist));
     }
 
 });
